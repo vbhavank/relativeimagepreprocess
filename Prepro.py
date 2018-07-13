b313dfa4d98dc5d3a3119472db3f6ea0fcde32e8
Bhavan <33493110+vbhavank@users.noreply.github.com>
2018-07-13 10:21:40 -0400

    Update Prepro.py

--- Prepro.py
+++ Prepro.py
@@ -91,15 +91,15 @@ plt.imshow(syn)
 
 #For processing a complete dataset in one go
 """
-name=0
-real_image_stack = np.stack([np.expand_dims(((Image.open(filename).resize((55,35)))),-1) for filename in glob.glob('link/to/real/*.png')],0)       #Change link to the folder containing 'ALL' the real imagery you have for reference
-real_image_stack=np.squeeze(real_image_stack)	#My data had unwanted channels i.e [6322,224,224,3,1] ,comment out incase your data imports in the form [6322,224,224,3]
+real_image_stack = np.stack([np.expand_dims(((Image.open(filename).resize((55,35)))),-1) for filename in glob.glob('/link/to/real/*.png')],0)
+real_image_stack=np.squeeze(real_image_stack)
 real_image_stack=cv2.normalize(real_image_stack.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
 flag=(np.shape(real_image_stack))[0]
-print(np.shape(real_image_stack))
-for filename in glob.glob('link/to/synthetic/*.PNG'): #Change to synthetic directory
+name=0
+for filename in glob.glob('/link/to/syn/directory/*.PNG'):
     normali=min_max_allscale(filename,real_image_stack,flag)
-    scipy.misc.imsave('/home/local/KHQ/bhavan.vasu/elm/out/july12/chip{}.png'.format(name),  normali)  #Location of the folder you want your preprocessed images to be saved
-    name=i+1
-print(ff)
+    scipy.misc.imsave('/link/to/save/dir/chip{}.png'.format(name),  normali)
+    name=name+1
+    print(name)
+print(name)
 """
